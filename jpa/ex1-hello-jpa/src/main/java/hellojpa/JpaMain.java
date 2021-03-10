bae2c43df2a1ff864935b9793a9ebcdf56ca1a85
make10in3 <make10in3@gmail.com>
2021-03-10 15:34:53 +0900

    [#6] 문제 발생시 롤백

--- jpa/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ jpa/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -13,13 +13,17 @@ public class JpaMain {
 
         tx.begin();
 
-        Member member = new Member();
-        member.setId(1L);
-        member.setName("HelloA");
-
-        em.persist(member);
-        tx.commit();
-        em.close();
+        try {
+            Member member = new Member();
+            member.setId(1L);
+            member.setName("HelloA");
+            em.persist(member);
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
         emf.close();
 
     }
