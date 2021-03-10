c75c910120d0cb57e516caaac0eb65448de00234
make10in3 <make10in3@gmail.com>
2021-03-10 20:30:32 +0900

    [#14] 더티체킹
    변경감지기능으로 엔티티수정

--- jpa/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ jpa/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -15,12 +15,10 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Member member1 = new Member(150L, "A");
-            Member member2 = new Member(160L, "B");
+            // 
+            Member member = em.find(Member.class, 150L);
+            member.setName("C");
 
-            em.persist(member1);
-            em.persist(member2);
-            System.out.println("===================");
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
