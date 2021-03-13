1ec8c5c9b883ab580083afb0e0c4bef7cd5fec17
make10in3 <make10in3@gmail.com>
2021-03-13 19:28:37 +0900

    [#18] 엔티티와 테이블 어노테이션 설명

--- jpa/ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ jpa/ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -1,7 +1,10 @@
 package hellojpa;
 import javax.persistence.Entity;
 import javax.persistence.Id;
-@Entity
+import javax.persistence.Table;
+
+@Entity(name="Member") // @Entity가 붙은 클래스를 엔티티라 부름, JPA내부에서 사용하는 이름이지만 기본으로 해놔야지 안헷갈린다.
+@Table(name="MBR") // 엔티티와 매핑 테이블 지정
 public class Member {
     @Id
     private Long id;
