27723c769621b4f85bcaac382f26c63342a49a15
make10in3 <make10in3@gmail.com>
2021-03-13 23:59:59 +0900

    [#21] identity 전략 / sequence 전략

--- jpa/ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ jpa/ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -6,9 +6,32 @@ import java.util.Date;
 
 @Entity(name="Member") // @Entity가 붙은 클래스를 엔티티라 부름, JPA내부에서 사용하는 이름이지만 기본으로 해놔야지 안헷갈린다.
 @Table(name="MBR") // 엔티티와 매핑 테이블 지정
+@SequenceGenerator(
+        name = "MEMBER_SEQ_GENERATOR",
+        sequenceName = "MEMBER_SEQ", //매핑할 데이터베이스 시퀀스 이름
+        initialValue = 1, allocationSize = 1)
 public class Member {
-    @Id //pk 매
+    @Id // 직접 할당
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "MEMBER_SEQ_GENERATOR") // 자동 생성
+    // GenerationType.IDENTITY : mysql auto increment, 방언에 따라서 자동 지정
+    // GenerationType.SEQUENCE : oracle 시퀀스 오브젝트 생성
     private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 
     @Column(name = "name", updatable = false, nullable = false, unique = false, length = 10, columnDefinition = "varchar(100) default 'EMPTY'")
     // unique 를 true 로 설정하면 알수없는 랜던값이 저장되기때문에 운영할때 어렵
