5ad3e88d361127b1940c2f7b31438f3ceba6c64d
make10in3 <make10in3@gmail.com>
2021-03-14 21:11:50 +0900

    [#24] 양뱡향 연관관계 설정

--- jpa/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ jpa/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -24,17 +24,15 @@ public class JpaMain {
             member.setTeam(team);
             em.persist(member);
 
-            // 디비에서 가져오는 것을 보고싶을 때
-            em.flush(); // 영속성 컨텍스트에 있는 디비쿼리를 디비에 다 전송해서 싱크를 맞춰놓고
-            em.clear(); // 영속성 컨텍스트 초기화
-
             Member findMember = em.find(Member.class, member.getId());
             Team findTeam = findMember.getTeam();
+
             System.out.println(findTeam.getName());
 
             // 새로운 팀으로 설정하고 싶을 때
             Team newTeam = em.find(Team.class, 100L);
             findMember.setTeam(newTeam);
+
 
             // 양방향 연관 관계 설정
             List<Member> members = findMember.getTeam().getMembers();
