5207fa92d94dc29eef629e0ee9f592289e8504b6
make10in3 <make10in3@gmail.com>
2021-03-20 16:36:43 +0900

    [#40] 쿼리 파라미터 예제 코드

--- jpa/hellojpa/src/main/java/jpql/JpaMain.java
+++ jpa/hellojpa/src/main/java/jpql/JpaMain.java
@@ -16,19 +16,10 @@ public class JpaMain {
             member.setUsername("member1");
             em.persist(member);
 
-            TypedQuery<Member> query1 = em.createQuery("select m from Member m", Member.class);
-            List<Member> resultList = query1.getResultList();
-
-            for (Member member1 : resultList) {
-                System.out.println("member1 = " + member1);
-            }
-
-            TypedQuery<Member> query = em.createQuery("select m from Member m where m.id=2L", Member.class);
-            Member result = query.getSingleResult();
-            // Spring Data JPA -> 결과가 없으면 EXCEPTION 터지는 것을 보완
-            // 지금 이 코드는 표준 스펙
-            System.out.println("result = " + result);
-
+            TypedQuery<Member> query1 = em.createQuery("select m from Member m where m.username = :username", Member.class);
+            query1.setParameter("username", "member1");
+            Member singleResult = query1.getSingleResult();
+            System.out.println("singleResult = " + singleResult.getUsername());
 
             tx.commit();
 
