a14747c8f914461353ac3976807d2091b91e6cb5
make10in3 <make10in3@gmail.com>
2021-03-20 16:14:04 +0900

    [#40] 기본 테이블 생성

---
+++ jpa/hellojpa/src/main/java/jpql/Team.java
@@ -0,0 +1,41 @@
+package jpql;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Team {
+
+    @Id @GeneratedValue
+    @Column(name="TEAM_ID")
+    private Long id;
+    private String username;
+
+    @OneToMany(mappedBy = "team")
+    private List<Member> members = new ArrayList<>();
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public List<Member> getMembers() {
+        return members;
+    }
+
+    public void setMembers(List<Member> members) {
+        this.members = members;
+    }
+}
