9e2f8afa17216b5ce50c6a461b0aa4e456954a59
make10in3 <make10in3@gmail.com>
2021-03-21 17:45:27 +0900

    [#41] 사용자 정의 함수 호출 예제

--- jpa/hellojpa/src/main/java/jpql/JpaMain.java
+++ jpa/hellojpa/src/main/java/jpql/JpaMain.java
@@ -13,41 +13,26 @@ public class JpaMain {
 
         try {
 
-            Team team = new Team();
-            team.setName("teamA");
-            em.persist(team);
+            Member member1 = new Member();
+            member1.setUsername("A");
+            em.persist(member1);
 
-            Member member = new Member();
-            member.setUsername("teamA");
-            member.setAge(10);
-            member.setTeam(team);
-
-            em.persist(member);
+            Member member2 = new Member();
+            member2.setUsername("B");
+            em.persist(member2);
 
             em.flush();
             em.clear();
 
-            // inner 조인 - inner 생략가능
-            String query1 = "select m from Member m inner join m.team t";
-            // outer 조인 - outer 생략 가능
-            String query2 = "select m from Member m left outer join m.team t";
-            // 세타 조인 - 상관없는 두 테이블 조인
-            String query3 = "select m from Member m , Team t where m.username = t.name";
+            String query = "select function('group_concat', m.username) from Member m";
+            // "select group_concat(m.username) from Member m" - hql 지원
 
-            // ON 절
-            // 1. 조인 대상 필터링
-            String query4 = "select m from Member m left join m.team t on t.name = 'A'";
-            // 2. 연관관계과 없는 엔티티 외부 조인
-            String query = "select m from Member m left join Team t on m.username = t.name";
-
-            List<Member> result = em.createQuery(query, Member.class)
+            List<String> result = em.createQuery(query, String.class)
                     .getResultList();
 
-            System.out.println("result.size() = " + result.size());
-            for (Member member1 : result) {
-                System.out.println("member1 = " + member1);
+            for (String s : result) {
+                System.out.println("s = " + s);
             }
-
             tx.commit();
 
         } catch(Exception e) {
