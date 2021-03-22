54279708893c919e94e9bd14c703fb077e504fb1
make10in3 <make10in3@gmail.com>
2021-03-22 16:33:50 +0900

    [#41] 지연로딩으로 인한 문제 코드

--- jpa/hellojpa/src/main/java/jpql/JpaMain.java
+++ jpa/hellojpa/src/main/java/jpql/JpaMain.java
@@ -12,27 +12,47 @@ public class JpaMain {
         tx.begin();
 
         try {
+            Team team1 = new Team();
+            team1.setName("팀A");
+            em.persist(team1);
+
+            Team team2 = new Team();
+            team2.setName("팀B");
+            em.persist(team2);
 
             Member member1 = new Member();
-            member1.setUsername("A");
+            member1.setUsername("회원1");
+            member1.setTeam(team1);
             em.persist(member1);
 
             Member member2 = new Member();
-            member2.setUsername("B");
+            member2.setUsername("회원2");
+            member2.setTeam(team1);
             em.persist(member2);
+
+            Member member3 = new Member();
+            member3.setUsername("회원2");
+            member3.setTeam(team2);
+            em.persist(member3);
 
             em.flush();
             em.clear();
 
-            String query = "select function('group_concat', m.username) from Member m";
-            // "select group_concat(m.username) from Member m" - hql 지원
+            String query = "select m from Member m";
 
-            List<String> result = em.createQuery(query, String.class)
+            List<Member> result = em.createQuery(query, Member.class)
                     .getResultList();
-
-            for (String s : result) {
-                System.out.println("s = " + s);
+            for (Member member : result) {
+                System.out.println("member = " + member);
+                System.out.println("member.getTeam().getName() = " + member.getTeam().getName());
             }
+
+            // 지연로딩 때문에 호출될 떄마다 쿼리문 날림
+            // 회원1, 팀A(SQL)
+            // 회원2, 팀A(1차캐시)
+            // 회원3, 팀B(SQL)
+            // 회원 100명 -> N+1 조회 -> 성능 문제
+
             tx.commit();
 
         } catch(Exception e) {
